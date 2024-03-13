397a0718311c8d11df6d607c58cbb3ed37fa9400
trinebra <157688079+trinebra@users.noreply.github.com>
2024-03-13 11:04:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// Kalkuler snittet:
+// Lag en funksjon som tar i mot et array av nummere som parameter.
+// Funksjonen skal returne gjennomsnittet av tallene i arrayet, og svaret skal kunne vises i console eller på en nettside
+
+
+//Modell
+let inputNumbers = [];
+
+//View
+updateView()
+function updateView() {
+    document.getElementById("app").innerHTML = /*HTML*/`
+    <br/>
+    <h1>Kalkuler Gjennomsnittet!</h1>
+    <br>
+    <h3>Numrene som skal kaluleres:</h3>
+    <div>${inputNumbers}</div>
+    <br>
+    <input id="inputNumbers" onchange="userInputNumbers(this.value)" type="number">
+    <br/>
+    <button onclick="calculateNumbers()">Kalkuler gjennomsnittet:</button>
+    <br/>
+    <h3>Gjennomsnittet:</h3>
+    <br/>
+    <div>${calculateNumbers(inputNumbers)}</div>
+    
+    
+    `;
+    // document.getElementById("inputNumbers").focus();
+}
+
+//Control
+function userInputNumbers(value) {
+    inputNumbers.push(value);
+    updateView()
+}
+
+function calculateNumbers(inputNumbers) {
+    let total = '';
+    let average = '';
+    for (let i = 0; i < inputNumbers.length; i++) {
+        total += inputNumbers[i];
+    }
+    // average = total / inputNumbers.length;
+    console.log(total)
+    return total / inputNumbers.length;
+
+}
+
